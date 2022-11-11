d6f2e4723aae02ec0024e2ff7a7374d3fbf205d1
cheesy d <cheesyd64@hotmail.com>
2022-11-11 12:39:10 -0800

    Completed Validation and styling

--- js/main.js
+++ js/main.js
@@ -13,12 +13,42 @@ window.onload = function () {
     addItem.onclick = main;
 };
 function main() {
-    if (isValid()) {
-        displayToDoItems(getToDoItem());
+    var entry = getToDoItem();
+    if (isValid(entry)) {
+        displayToDoItems(entry);
     }
 }
-function isValid() {
-    return true;
+function isValid(item) {
+    var valid = true;
+    var taskOkay = true;
+    var dateOkay = true;
+    if (item.task == "" || /^[0-9., ]+$/.test(item.task)) {
+        var taskError = document.createElement("h3");
+        if (fromId("task-error") == null) {
+            taskError.setAttribute("id", "task-error");
+            taskError.innerText = "Enter a task, and not just a number.";
+            (fromId("entry-data")).appendChild(taskError);
+        }
+        valid = false;
+        taskOkay = false;
+    }
+    if ((fromId("due-date")).value == "" || !(Date.parse(fromId("due-date").value))) {
+        var dateError = document.createElement("h3");
+        if (fromId("date-error") == null) {
+            dateError.setAttribute("id", "date-error");
+            dateError.innerText = "Due date required";
+            (fromId("entry-data")).appendChild(dateError);
+        }
+        valid = false;
+        dateOkay = false;
+    }
+    if (fromId("task-error") != null && taskOkay == true) {
+        fromId("task-error").remove();
+    }
+    if (fromId("date-error") != null && dateOkay == true) {
+        fromId("date-error").remove();
+    }
+    return valid;
 }
 function getToDoItem() {
     var item = new ToDoItem();
@@ -28,18 +58,25 @@ function getToDoItem() {
     return item;
 }
 function displayToDoItems(item) {
-    var toDoItem = document.createElement("h3");
-    toDoItem.innerText = item.task + " " + item.dueDate.toDateString();
+    var toDoEntry = document.createElement("p");
+    toDoEntry.innerText = item.task + " by " + item.dueDate.toDateString();
     var itemDiv = document.createElement("div");
+    itemDiv.onclick = markCompleteOrNot;
     itemDiv.classList.add("todo");
     if (item.isComplete) {
         itemDiv.classList.add("completed");
     }
-    itemDiv.appendChild(toDoItem);
+    itemDiv.appendChild(toDoEntry);
     if (item.isComplete) {
         (fromId("complete-items")).appendChild(itemDiv);
     }
     else {
         (fromId("incomplete-items")).appendChild(itemDiv);
     }
+}
+function markCompleteOrNot() {
+    var itemDiv = this;
+    this.classList.add("completed");
+    var completedItems = fromId("complete-items");
+    completedItems.appendChild(itemDiv);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -19,20 +19,55 @@ window.onload = function():void{
 }
 
 function main():void{
-    if(isValid()){
-        displayToDoItems(getToDoItem())
+    let entry:ToDoItem = getToDoItem();
+
+    if(isValid(entry)){
+        displayToDoItems(entry);
     }
 }
 
 /**
  * check if data is valid
  */
-function isValid():boolean{
-    //task validation
-   
+function isValid(item:ToDoItem):boolean{
+    let valid = true;
+    let taskOkay:boolean = true;
+    let dateOkay:boolean = true;
 
+    //task validation
+    if(item.task == "" || /^[0-9., ]+$/.test(item.task)){
+        let taskError:HTMLElement = document.createElement("h3");
+
+        if(fromId("task-error") == null){
+            taskError.setAttribute("id", "task-error");
+            taskError.innerText = "Enter a task, and not just a number.";
+            (fromId("entry-data")).appendChild(taskError);
+        }
+        valid = false;
+        taskOkay = false;
+    }
+   
     //date validation
-    return true;
+    if( (fromId("due-date")).value == "" || !(Date.parse(fromId("due-date").value))){
+        let dateError:HTMLElement = document.createElement("h3");
+        
+        if(fromId("date-error") == null){
+            dateError.setAttribute("id", "date-error");
+            dateError.innerText = "Due date required";
+            (fromId("entry-data")).appendChild(dateError);
+        }
+        valid = false;
+        dateOkay = false;
+    }
+   
+    //remove error if entry is valid
+    if(fromId("task-error") != null && taskOkay == true){
+        fromId("task-error").remove();
+    }
+    if(fromId("date-error") != null && dateOkay == true){
+        fromId("date-error").remove();
+    }
+    return valid;
 }
 
 /**
@@ -56,24 +91,34 @@ function getToDoItem():ToDoItem{
 function displayToDoItems(item:ToDoItem):void{
 
     //make an h3 and set text to task and due date 
-    let toDoItem:HTMLElement = document.createElement("h3");
-    toDoItem.innerText = item.task + " " + item.dueDate.toDateString();
+    let toDoEntry:HTMLElement = document.createElement("p");
+    toDoEntry.innerText = item.task + " by " + item.dueDate.toDateString();
 
-    //div class="completed" and "todo"
+    //div class="completed" and "todo" and click
     let itemDiv = document.createElement("div");
+    itemDiv.onclick = markCompleteOrNot;
     itemDiv.classList.add("todo");
     if(item.isComplete){
         itemDiv.classList.add("completed");
     }
 
-    itemDiv.appendChild(toDoItem);
+    //add entry to itemDiv
+    itemDiv.appendChild(toDoEntry);
 
+    //display itemDiv in appropriate list
     if(item.isComplete){
         (fromId("complete-items")).appendChild(itemDiv);
     }
     else{
         (fromId("incomplete-items")).appendChild(itemDiv);
     }
-    
+}
+
+function markCompleteOrNot(){
+    let itemDiv = <HTMLElement>this;
+    this.classList.add("completed");
+
+    let completedItems = fromId("complete-items");
+    completedItems.appendChild(itemDiv);
 }
 
